fbd26be516c37809964ce439fd4feea565fd4b7f
Damon Pham <damondpham@gmail.com>
2022-06-10 21:10:55 -0400

    Add the package skeleton

---
+++ R/data_setup.R
@@ -0,0 +1,10 @@
+#' Set up the data
+#' 
+#' Set up the data given the input type
+#' 
+#' @param ... Description
+#' @return The value
+#' @export
+data_setup <- function(...){
+  NULL
+}
---
+++ R/fMRIComBat.R
@@ -0,0 +1,10 @@
+#' fMRI ComBat
+#' 
+#' Batch correction for fMRI data
+#' 
+#' @param ... Description
+#' @return The value
+#' @export
+fMRIComBat <- function(...){
+  NULL
+}
